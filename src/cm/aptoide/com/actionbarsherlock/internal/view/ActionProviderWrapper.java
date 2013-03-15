20429a9926f7d44607a7d6b36a287298784fd666
Teresa Futscher de Deus <teresa.deus@aptoide.com>
2013-03-15 17:07:10 +0000

    set aptoide theme class, theme, styles, attrs, actionbarsherlock prepared
    git-svn-id: http://aptoide.org/repo/libraries/aptoide-support-libs@1156 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ src/cm/aptoide/com/actionbarsherlock/internal/view/ActionProviderWrapper.java
@@ -0,0 +1,41 @@
+package cm.aptoide.com.actionbarsherlock.internal.view;
+
+import cm.aptoide.com.actionbarsherlock.internal.view.menu.SubMenuWrapper;
+import cm.aptoide.com.actionbarsherlock.view.ActionProvider;
+
+import android.view.View;
+
+public class ActionProviderWrapper extends android.view.ActionProvider {
+    private final ActionProvider mProvider;
+
+
+    public ActionProviderWrapper(ActionProvider provider) {
+        super(null/*TODO*/); //XXX this *should* be unused
+        mProvider = provider;
+    }
+
+
+    public ActionProvider unwrap() {
+        return mProvider;
+    }
+
+    @Override
+    public View onCreateActionView() {
+        return mProvider.onCreateActionView();
+    }
+
+    @Override
+    public boolean hasSubMenu() {
+        return mProvider.hasSubMenu();
+    }
+
+    @Override
+    public boolean onPerformDefaultAction() {
+        return mProvider.onPerformDefaultAction();
+    }
+
+    @Override
+    public void onPrepareSubMenu(android.view.SubMenu subMenu) {
+        mProvider.onPrepareSubMenu(new SubMenuWrapper(subMenu));
+    }
+}
---
+++ src/cm/aptoide/com/actionbarsherlock/internal/view/View_HasStateListenerSupport.java
@@ -0,0 +1,6 @@
+package cm.aptoide.com.actionbarsherlock.internal.view;
+
+public interface View_HasStateListenerSupport {
+    void addOnAttachStateChangeListener(View_OnAttachStateChangeListener listener);
+    void removeOnAttachStateChangeListener(View_OnAttachStateChangeListener listener);
+}
